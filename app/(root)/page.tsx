1e2c5db05f465001039b0ad063cb326f8f743de4
danielaguardad0 <danielamguatdado@gmail.com>
2025-07-31 21:52:04 -0600

    E-Commerce-Parcial

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from '@/lib/actions/product.actions'
+import ClientHome from '@/components/cliente-home'
+
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts()
+  const featuredProducts = await getFeaturedProducts()
+
+  return (
+    <ClientHome
+      latestProducts={latestProducts}
+      featuredProducts={featuredProducts}
+    />
+  )
+}
+
+export default Homepage
